24889b116137208bc10f524ed88d85117d9de56b
ShreyaskarSingh <46413191+ShreyaskarSingh@users.noreply.github.com>
2019-02-08 19:33:35 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,26 +6,30 @@
 ## Write a short comment describing this function
 
 ## This function creates a special "matrix" object that can cache its inverse
+
 makeCacheMatrix <- function(x = matrix()) {
-inv <- NULL
- set <- function(y){
-         x <<- y
-         inv <<- NULL
- }
+ inv <- NULL                             ## initialize inv as NULL; will hold value of matrix inverse 
+ 
+    set <- function(y) {                    ## define the set function to assign new 
+        x <<- y                             ## value of matrix in parent environment
+        inv <<- NULL                        ## if there is a new matrix, reset inv to NULL
+    }
+ 
+    get <- function(){ 
+     x 
+    }                                             ## define the get fucntion - returns value of the matrix argument
 
- get <- function(){
-         x
- }
- setinverse <- function(inverse){
-         inv <<-inverse
- }
-
- getinverse <-function(){
-         inv
- }
-
- list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
-
+    setinverse <- function(inverse) {
+     inv <<- inverse
+    }                                             ## assigns value of inv in parent environment
+ 
+    getinverse <- function() {
+     inv
+    }                                             ## gets the value of inv where called
+ 
+ 
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer 
+                                                                                  ## to the functions with the $ operator
 }
 
 
@@ -38,12 +42,16 @@ inv <- NULL
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getinverse()
-    if(!is.null(inv)) {
+    if(!is.null(inv)) {             
         message("getting cached data")
         return(inv)
-    }
+    }                                              # checks for the null condition matrix
+ 
     data <- x$get()
-    inv <- solve(data, ...)
+ 
+    inv <- solve(data, ...)                        # solves  the inverse of the given matrix
+ 
     x$setinverse(inv)
-    inv
+ 
+    inv                                            # returns the result
 }
